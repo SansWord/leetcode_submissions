77f6c003e422d29111f79f3d9689cfd02d1ca6a8
SansWord <sansword@gmail.com>
2015-10-31 10:17:07 +0000

    [LeetCode Sync] Runtime - 5 ms (3.07%), Memory - N/A (N/A)

---
+++ submissions/0202-happy-number/solution.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author sansword, @date 2015/10/31 17:33
+ */
+public class Solution {
+    public boolean isHappy(int input) {
+        return isHappy(input, new HashSet<Integer>());
+    }
+
+    private boolean isHappy(int current, Set<Integer> visited) {
+        if(current == 1) {
+            return true;
+        } else {
+            if(visited.contains(current)) {
+                return false;
+            } else {
+                visited.add(current);
+                return isHappy(nextInteger(current), visited);
+            }
+        }
+    }
+
+    private int nextInteger(int input) {
+        int result = 0;
+        while(input >= 10) {
+            int digit = input % 10;
+            result += digit * digit;
+            input = input / 10;
+        }
+        result += input * input;
+        return result;
+    }
+}
+
