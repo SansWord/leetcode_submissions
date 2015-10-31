9e48e3ae46da74bb77bba73720e1ee50da7e7c5a
SansWord <sansword@gmail.com>
2015-10-31 10:02:40 +0000

    [LeetCode Sync] Runtime - 2 ms (1.40%), Memory - N/A (N/A)

---
+++ submissions/0258-add-digits/solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int addDigits(int num) {
+        if(num < 10) {
+            return num;
+        } else {
+            return addDigits(num/10 + num % 10);
+        }
+    }
+}
