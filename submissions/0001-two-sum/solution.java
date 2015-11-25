60d588fa916ecbdbb1cab75085d59fa1e07a66c0
SansWord <sansword@gmail.com>
2015-11-25 07:36:46 +0000

    [LeetCode Sync] Runtime - 41 ms (37.88%), Memory - N/A (N/A)

---
+++ submissions/0001-two-sum/solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int inputLength = nums.length;
+        for(int i = 0; i < inputLength; i++) {
+            for(int j = i+1; j < inputLength; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[] {i+1, j+1};
+                }
+            }
+        }
+        return new int[] {0, 0};
+    }
+}
