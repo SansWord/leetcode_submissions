ff5b0438a6dc2c37e55e50a9185f749ec5f1fd49
SansWord <sansword@gmail.com>
2017-01-28 10:52:16 +0000

    [LeetCode Sync] Runtime - 1 ms (16.68%), Memory - N/A (N/A)

---
+++ submissions/0114-flatten-binary-tree-to-linked-list/solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        } else {
+            flatten(root.left);
+            flatten(root.right);
+            root.right = merge(root.left, root.right);
+            root.left = null;
+        }
+    }
+    
+    private TreeNode merge(TreeNode flattenLeft, TreeNode flattenRight) {
+        if (flattenLeft == null) {
+            return flattenRight;    
+        } else {
+            if (flattenRight != null) {
+                TreeNode current = flattenLeft;
+                while(current.right != null) {
+                    current = current.right;
+                }
+                current.right = flattenRight;
+            }
+            return flattenLeft;
+        }
+    }
+}
