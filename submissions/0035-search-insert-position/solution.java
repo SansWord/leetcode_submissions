6b5c26ae0672601816e9f5b0e3516d98bb0460fb
SansWord <sansword@gmail.com>
2021-11-22 22:13:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.8 MB (100.00%)

--- submissions/0035-search-insert-position/solution.java
+++ submissions/0035-search-insert-position/solution.java
@@ -1,34 +1,22 @@
-public class Solution {
+class Solution {
     public int searchInsert(int[] nums, int target) {
-        return binarySearch(nums, target, 0, nums.length - 1);
+        return search(nums, target, 0, nums.length-1);
     }
-
-    private int binarySearch(int[] nums, int target, int start, int end) {
-        if (start > end) {
-            return start;
+    
+    private int search(int[] nums, int target, int left, int right) {
+        
+        if(left>right) {
+            return left;
+        }
+        int currentIdx = left + (right-left)/2;
+        int current = nums[currentIdx];
+        if(target==current) {
+            return currentIdx;
+        }
+        if(target < current) {
+            return search(nums, target, left, currentIdx-1);
         } else {
-            int startNum = nums[start];
-            int endNum = nums[end];
-            if (target <= startNum) {
-                return start;
-            }
-
-            if (endNum == target) {
-                return end;
-            }
-            if (endNum < target) {
-                return end + 1;
-            }
-
-            int pivot = (start + end) / 2;
-            int current = nums[pivot];
-            if (target == current) {
-                return pivot;
-            } else if (target < current) {
-                return binarySearch(nums, target, start + 1, pivot - 1);
-            } else { //if (target > current)
-                return binarySearch(nums, target, pivot + 1, end - 1);
-            }
+            return search(nums, target, currentIdx+1, right);
         }
     }
 }
