2122600bb2d33fb51e9084091a5b533698bfab13
SansWord <sansword@gmail.com>
2025-10-08 08:50:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (13.24%)

---
+++ submissions/0145-binary-tree-postorder-traversal/solution.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        self.walk(root, result)
+        return result
+
+    def walk(self, root: Optional[TreeNode], result: List[int]) -> None:
+        if not root:
+            return
+        
+        self.walk(root.left, result)
+        self.walk(root.right, result)
+        result.append(root.val)
