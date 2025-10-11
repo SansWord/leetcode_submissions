7abb67707792234c5ee674f9348ccad19df7bf37
SansWord <sansword@gmail.com>
2025-10-11 10:19:42 +0000

    [LeetCode Sync] Runtime - 2 ms (35.12%), Memory - 18 MB (25.58%)

---
+++ submissions/1651-shuffle-string/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        result = [ None for i in range(len(indices))]
+        for i in range(len(indices)):
+            result[indices[i]] = s[i]
+
+        return "".join(result)
+
+        
