1ffce4d45c397b4b6e144b9f19d0ba459870dbea
SansWord <sansword@gmail.com>
2025-10-13 19:35:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (43.65%)

--- submissions/0022-generate-parentheses/solution.py
+++ submissions/0022-generate-parentheses/solution.py
@@ -1,31 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        self.mem = [None] * (n+1)
-        return self.calculate(n)
+        self.result = []
+        self.stack = []
+        self.n = n
+        self.calculate(0, 0)
+        return self.result
 
-    def calculate(self, n: int) -> List[str]:
-        if n == 0:
-            return [""]
-        if n == 1:
-            return ["()"]
-
-        if self.mem[n]:
-            return self.mem[n]
-
-        else:
-            result = []
-            
-            # iterate over for the len of first parentheses
-            # it could be from 1*2 to n*2
-            # hence it looks like f"({preP}){postP}" for preP from 0*2 to (n-1)*2
-            # and length of postP is n - 1 - preP
-
-            for i in range(n-1, -1, -1):
-                prePs = self.calculate(i)
-                postPs = self.calculate(n-i-1)
-
-                for preP in prePs:
-                    for postP in postPs:
-                        result.append(f"({preP}){postP}")
-
-        return result
+    def calculate(self, openP: int, closeP: int) -> None:
+        if openP == closeP and openP == self.n:
+            self.result.append("".join(self.stack))
+            return
+        
+        if openP < self.n:
+            self.stack.append("(")
+            self.calculate(openP+1, closeP)
+            self.stack.pop()
+        
+        if openP > closeP:
+            self.stack.append(")")
+            self.calculate(openP, closeP+1)
+            self.stack.pop()
