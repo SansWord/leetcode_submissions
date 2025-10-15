de3d7236e90c1fc4c11b355adb0a06384c1377c8
SansWord <sansword@gmail.com>
2025-10-15 08:44:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (64.96%)

---
+++ submissions/0225-implement-stack-using-queues/solution.py
@@ -0,0 +1,29 @@
+class MyStack:
+
+    def __init__(self):
+        self.data = deque()
+
+    def push(self, x: int) -> None:
+        self.data.append(x)
+        for i in range(len(self.data)-1):
+            self.data.append(self.data.popleft())
+        
+
+    def pop(self) -> int:
+        return self.data.popleft()
+        
+
+    def top(self) -> int:
+        return self.data[0]
+        
+
+    def empty(self) -> bool:
+        return len(self.data) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
