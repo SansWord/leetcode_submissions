46c800fbf3a88dc65200b83f0884f390f6d09a2f
SansWord <sansword@gmail.com>
2025-10-15 10:18:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (83.79%)

---
+++ submissions/0231-power-of-two/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        while n > 0:
+            if n % 2 == 1:
+                if n != 1:
+                    return False
+            n //= 2
+        return True
+        
