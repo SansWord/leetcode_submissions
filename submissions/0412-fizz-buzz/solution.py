11f982bcecb64becfdb6f4dcbd76e07e3655702d
SansWord <sansword@gmail.com>
2025-10-17 17:31:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.4 MB (42.80%)

---
+++ submissions/0412-fizz-buzz/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        res = []
+        for i in range(n):
+            k = i + 1
+            if k % 15 == 0:
+                res.append("FizzBuzz")
+            elif k % 3 == 0:
+                res.append("Fizz")
+            elif k % 5 == 0:
+                res.append("Buzz")
+            else:
+                res.append(str(k))
+        
+        return res
