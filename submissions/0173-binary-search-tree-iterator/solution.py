6c422ed8a10def17ba7a38573ae6d1a8bb6c3273
SansWord <sansword@gmail.com>
2026-03-16 21:58:39 +0000

    [LeetCode Sync] Runtime - 12 ms (20.30%), Memory - 25.5 MB (64.49%)

---
+++ submissions/0173-binary-search-tree-iterator/solution.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.root = root
+        self.stack = []
+        self.addLeft(root)
+    
+    def addLeft(self, node):
+        
+        while node:
+            self.stack.append(node)
+            node = node.left
+    
+
+    def next(self) -> int:
+        curr = self.stack.pop()
+        result = curr.val
+
+        self.addLeft(curr.right)
+
+        return result
+        
+
+        
+
+    def hasNext(self) -> bool:
+        return len(self.stack) != 0
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
